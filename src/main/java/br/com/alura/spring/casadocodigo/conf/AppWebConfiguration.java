d00c966de4775c84b2894397b39533c4a6f9fbd8
Rafael Carvalho <crcarvalho@outlook.com>
2019-02-13 12:49:58 -0300

    Incluindo Bootstrap na página

--- src/main/java/br/com/alura/spring/casadocodigo/conf/AppWebConfiguration.java
+++ src/main/java/br/com/alura/spring/casadocodigo/conf/AppWebConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -29,6 +30,13 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	public void addFormatters(FormatterRegistry registry) {
 		registry.addConverter(new TipoStatusConverter());
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**")
+		.addResourceLocations("/WEB-INF/resources/");
+	}
+	
 	
 	
 	
